b6c303949ab5cda03cc9cc34b4ad1d43e9844d52
kelvao-09 <kelvin.silva@grupobrisanet.com.br>
2026-01-09 22:33:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,12 @@ st.write("Este site foi criado usando apenas Python e Streamlit.")
 nome = st.text_input("Digite seu nome:")
 if nome:
     st.write(f"Olá {nome}, seu app está funcionando!")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.header("Coluna 1")
+    st.button("Botão na esquerda")
+
+with col2:
+    st.header("Coluna 2")
+    st.write("Texto na direita")
